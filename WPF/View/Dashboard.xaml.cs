01886c91a3fe4f52cd0549257ae02b25d89e280e
LUCKY ADHIKRISNA WIRASAKTI <lucky.30@students.amikom.ac.id>
2018-10-26 13:58:11 +0700

    login and dashboard

---
+++ WPF/View/Dashboard.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace WPF.View
+{
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Hide();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ WPF/View/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using WPF.View;
+
+namespace WPF
+{   public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtUsername.Text == "Lucky")
+            {
+                if(txtPassword.Text == "amikom")
+                {
+                    Dashboard dashboard = new Dashboard();
+                    dashboard.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Kata Sandi Tidak Cocok", "Peringatan!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Akun Tidak Ditemukan", "Peringatan", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
